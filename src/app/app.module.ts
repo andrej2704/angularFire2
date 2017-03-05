f1ac77f1c11ee3648ebfad83f0dc7c6871d95ef2
basementcz <you@example.com>
2017-03-05 12:52:12 +0100

    configured firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBIGi5op909-sFe6DvDg7qEBLLifpRq6Ro',
+  authDomain: 'angularfire2-98b10.firebaseapp.com',
+  databaseURL: 'https://angularfire2-98b10.firebaseio.com',
+  storageBucket: 'angularfire2-98b10.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
