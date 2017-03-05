53f18e7c1c0787be2b6bbd03caff783ab9d92a24
basementcz <you@example.com>
2017-03-06 00:20:33 +0100

    add filtering by category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,10 +22,10 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     this._firebaseService.getBusinesses().subscribe(businesses => {
       this.businesses = businesses;
-    })
+    });
     this._firebaseService.getCategories().subscribe(categories => {
       this.categories = categories;
-    })
+    });
   }
 
   changeState(state, key) {
@@ -35,5 +35,11 @@ export class AppComponent implements OnInit {
       this.activeKey = key;
     }
     this.appState = state;
+  }
+
+  filterCategory(category){
+    this._firebaseService.getBusinesses(category).subscribe(businesses => {
+      this.businesses = businesses;
+    });
   }
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -13,8 +13,18 @@ export class FireBaseService{
 
     }
 
-    getBusinesses(): FirebaseListObservable<Business[]>{
-        this.businesses = this._af.database.list('/businesses') as FirebaseListObservable<Business[]>;
+    getBusinesses(category:string = null): FirebaseListObservable<Business[]>{
+        if (category != null) {
+            this.businesses = this._af.database.list('/businesses', {
+                query: {
+                    orderByChild: 'category',
+                    equalTo: category
+                }
+            }) as FirebaseListObservable<Business[]>;
+
+        } else {
+            this.businesses = this._af.database.list('/businesses') as FirebaseListObservable<Business[]>;
+        }
         return this.businesses;
     }
 
